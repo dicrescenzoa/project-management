6ff0c0fb19e1809ea211fdea0ef06a0102f47f5e
Alessio Di Crescenzo <a.dicrescenzo@elsevier.com>
2021-02-15 18:18:28 +0100

    add crete project feature with h2 databaase

---
+++ src/main/java/com/hydra/pma/controllers/ProjectController.java
@@ -0,0 +1,35 @@
+package com.hydra.pma.controllers;
+
+import com.hydra.pma.dao.ProjectRepository;
+import com.hydra.pma.entities.Project;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import com.google.gson.Gson;
+
+@Controller
+@RequestMapping("/projects")
+public class ProjectController {
+
+    @Autowired
+    ProjectRepository projectRepo;
+
+    @GetMapping("/new")
+    public String displayProjectForm(Model model) {
+        Project aProject = new Project();
+        model.addAttribute("project", aProject);
+        return "new-project";
+    }
+
+    @PostMapping(value = "/save")
+    public String createProjectForm(Project project, Model model) {
+        Gson gson = new Gson();
+        String userJson = gson.toJson(project);
+        System.out.println(userJson);
+        projectRepo.save(project);
+        return "redirect:/projects/new";
+    }
+}
---
+++ src/main/java/com/hydra/pma/dao/ProjectRepository.java
@@ -0,0 +1,8 @@
+package com.hydra.pma.dao;
+
+import com.hydra.pma.entities.Project;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ProjectRepository extends CrudRepository<Project, Long> {
+
+}
