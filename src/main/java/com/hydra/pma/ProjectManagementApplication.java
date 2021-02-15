fb6c02a43cf9e1c77b735fda5b7e65d340f86854
Alessio Di Crescenzo <a.dicrescenzo@elsevier.com>
2021-02-15 16:12:27 +0100

    Init Spring Boot project

---
+++ src/main/java/com/hydra/pma/ProjectManagementApplication.java
@@ -0,0 +1,13 @@
+package com.hydra.pma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectManagementApplication.class, args);
+    }
+
+}
